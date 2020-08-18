4e49481f257e9398abb77555e0121921b6f05586
Eli <eyturner@umich.edu>
2020-08-18 16:45:20 -0700

    update git

--- gp-server/server.js
+++ gp-server/server.js
@@ -18,6 +18,12 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+const lessonsRouter = require('./routes/lessons');
+const usersRouter = require('./routes/users');
+
+app.use('/lessons', lessonsRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
